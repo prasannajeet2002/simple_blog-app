a7b390fbd0ae6c958c7d9aebf0aed2e061b8d980
prasannajeet2002 <75658251+prasannajeet2002@users.noreply.github.com>
2021-11-25 02:04:24 +0530

    Add files via upload

---
+++ Routes/article.js
@@ -0,0 +1,67 @@
+const express = require('express')
+const Article = require('./../models/articlesdb')
+const router = express.Router()
+
+router.get('/new', (req,res) => {
+    res.render('articles/new', {article: new Article() })
+})
+router.get('/edit/:id', async (req, res) => {
+    const article = await Article.findById(req.params.id)
+    res.render('articles/edit', {article: article})
+})
+router.get('/:id', async(req,res) => { 
+const article = await Article.findById(req.params.id)
+if (article==null) {
+    res.redirect('/')
+}
+res.render('articles/show' , {article:article})
+// res.send(article._id);
+}) 
+
+router.post('/', async(req,res) => {
+
+    let article = new Article({
+        title: req.body.title,
+        description: req.body.description,
+        markdown: req.body.markdown
+    })
+    try {
+    article = await article.save()
+    // res.send("blog saved");
+    // console.log(article);
+    res.status(201).redirect(`/articles/${article.id}`)
+} catch (e) { 
+    // console.log(e);
+   res.render('articles/new', {article: article})
+//    res.status(400).send(error) 
+
+}
+})
+router.put('/:id', async(req,res) => {
+await Article.findById(req.params.id)
+    let article = new Article({
+        title: req.body.title,
+        description: req.body.description,
+        markdown: req.body.markdown
+    })
+    try {
+    article = await article.save()
+    // res.send("blog saved");
+    // console.log(article);
+    res.status(201).redirect(`/articles/${article.id}`)
+} catch (e) { 
+    // console.log(e);
+   res.render('articles/edit', {article: article})
+//    res.status(400).send(error) 
+
+}
+})
+
+
+
+router.delete('/:id', async  (req, res) => {
+    await Article.findByIdAndDelete(req.params.id)
+    res.redirect('/')
+})
+
+module.exports = router
---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const articlerouter = require("./routes/article");
+const app = express();
+const Article = require('./models/articlesdb')
+const methodoverride = require('method-override')
+
+mongoose 
+  .connect("mongodb://localhost:27017/articles", { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => {
+    console.log("connection succesful");
+  })
+  .catch((error) => console.log(error));
+
+// app.use(express.json());
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false })); 
+app.use(methodoverride('_method'))
+
+
+app.get("/",async (req, res) => {
+  const article = await Article.find().sort({createdat:'desc'})
+  res.render("articles/index", { article: article });
+}); 
+
+app.use('/articles', articlerouter);
+app.listen(4000);
